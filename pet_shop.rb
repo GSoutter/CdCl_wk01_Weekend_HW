7d0c2a6fd898a7bc003c8417a523873cbb6dec72
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-13 14:45:23 +0000

    adds function: pets_sold, increase_pets_sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,6 +5,7 @@ def pet_shop_name(petshop_hash)
   return petshop_hash[:name]
 end
 
+
 def total_cash(petshop_hash)
   return petshop_hash[:admin][:total_cash]
 
@@ -14,4 +15,18 @@ end
 def add_or_remove_cash(petshop_hash, transaction_cash)
   petshop_hash[:admin][:total_cash] += transaction_cash
   return petshop_hash[:admin][:total_cash]
+end
+
+
+def pets_sold(petshop_hash)
+  return petshop_hash[:admin][:pets_sold]
+end
+
+def increase_pets_sold(petshop_hash, pets_sold_number)
+  petshop_hash[:admin][:pets_sold] += pets_sold_number
+  return petshop_hash[:admin][:pets_sold]
+end
+
+def stock_count(petshop_hash)
+
 end
