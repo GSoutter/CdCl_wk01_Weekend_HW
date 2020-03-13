d177c59d0000ce32975abb9f7013ea1a2f7fb2ea
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-13 14:15:15 +0000

    intial commit. Adds name and cash functions

---
+++ pet_shop.rb
@@ -0,0 +1,16 @@
+
+
+
+def pet_shop_name(petshop_hash)
+  return petshop_hash[:name]
+end
+
+def total_cash(petshop_hash)
+  return petshop_hash[:admin][:total_cash]
+
+end
+
+
+def add_or_remove_cash(petshop_hash, transaction_cash)
+  
+end
